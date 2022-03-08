12eb72320305fe8562f083ce665c1978cf265b66
CallingMyBluff <ismaeel.abuabdallah@gmail.com>
2022-03-08 02:41:03 +0100

    Handle exception in queries

--- src/controllers/autoComplete.ts
+++ src/controllers/autoComplete.ts
@@ -7,15 +7,19 @@ interface IAutoComplete {
 }
 
 async function completeText( text: string, language: string, limit: number = 20 ): Promise<IAutoComplete[]> {
-  const res = await Model.query(`
-    SELECT i18n.name as City, location.longitude as Longitude, location.latitude as Latitude from i18n
-      INNER JOIN location ON location.id = i18n.location_id
-      INNER JOIN language ON language.id = i18n.language_id
-      WHERE language.code = '${language}' AND i18n.name LIKE '%${text}%'
-      GROUP BY location.id
-      LIMIT ${limit};
-  `)
-  return res
+  try {
+    return await Model.query(`
+      SELECT i18n.name as City, location.longitude as Longitude, location.latitude as Latitude from i18n
+        INNER JOIN location ON location.id = i18n.location_id
+        INNER JOIN language ON language.id = i18n.language_id
+        WHERE language.code = '${language}' AND i18n.name LIKE '%${text}%'
+        GROUP BY location.id
+        LIMIT ${limit};
+    `)
+  }
+  catch {
+    return []
+  }
 }
 
 export default {
